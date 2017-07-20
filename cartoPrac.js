f34ef86e621fb2d57f5ef8bb8e588b9e827c26df
nbbennett <nbbennett@lbl.gov>
2017-07-20 15:09:11 -0700

    Website1

---
+++ cartoPrac.js
@@ -0,0 +1,41 @@
+//
+//  cartoPrac.js
+//  Carto.js environment
+//
+//  Created by loaner on 6/27/17.
+//
+//
+
+<!DOCTYPE html>
+<html>
+<head>
+<title>Lesson 1 | CARTO.js | CartoDB</title>
+<meta name="viewport" content="initial-scale=1.0, user-scalable=no" />
+<meta http-equiv="content-type" content="text/html; charset=UTF-8"/>
+<link rel="shortcut icon" href="https://carto.com/favicon.ico" />
+
+<!-- Map takes up full browser window -->
+<style>
+html, body, #map {
+height: 100%;
+padding: 0;
+margin: 0;
+}
+</style>
+
+<link rel="stylesheet" href="https://cartodb-libs.global.ssl.fastly.net/cartodb.js/v3/3.15/themes/css/cartodb.css" />
+</head>
+<body>
+<div id="map"></div>
+
+<!-- include cartodb.js library -->
+<script src="https://cartodb-libs.global.ssl.fastly.net/cartodb.js/v3/3.15/cartodb.js"></script>
+
+
+<!-- Drop your code between the script tags below! -->
+<script>
+
+</script>
+
+</body>
+</html>
